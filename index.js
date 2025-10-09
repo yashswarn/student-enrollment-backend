53061e887c5c4bef6572604ef7ebe9fcdd21c0c5
Yash Soni <yash27827@gmail.com>
2025-10-09 21:36:52 +0530

    backend

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 // }));
 
 app.use(cors({
-  origin: ['https://verdant-kataifi-997843.netlify.app', "http://localhost:4200"],
+  origin: 'https://verdant-kataifi-997843.netlify.app',
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
